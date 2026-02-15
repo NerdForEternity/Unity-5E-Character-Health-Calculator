ab858ab1c689747218aa094fabdb64333bae85d0
Jayden Currence <jaydencurrence@gmail.com>
2026-02-15 02:23:58 -0500

    barely started tbh

--- Assets/Scripts/SolutionTwo.cs
+++ Assets/Scripts/SolutionTwo.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 public class SolutionTwo : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private class playerCharacter
+    {
+
+        public float hp;
+        public bool isStout;
+        public bool isTough;
+
+        
+    }
+
+    [SerializeField] private playerCharacter player;
+
+    
     void Start()
     {
         
